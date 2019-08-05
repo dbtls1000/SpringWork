b50594fa265ee8e1fa0fd67b5ea6a09922ca528e
dbtls1000 <dbsqhtjs@naver.com>
2019-08-05 16:43:01 +0900

    2019-08-05 spring 학습

---
+++ spMVC_CRUD_06_BBS_03/src/main/java/com/biz/bbs/mapper/FileSQL.java
@@ -0,0 +1,20 @@
+package com.biz.bbs.mapper;
+
+import org.apache.ibatis.jdbc.SQL;
+
+public class FileSQL {
+	
+	
+	public String file_insert_sql() {
+		SQL sql = new SQL() {{
+			INSERT_INTO("tbl_bbs_file");
+			INTO_COLUMNS("file_seq").INTO_VALUES("SEQ_BBS_FILE.NEXTVAL");
+			// 게시글과 파일의 연광관계를 설정하는 키값
+			INTO_COLUMNS("file_bbs_seq").INTO_VALUES("#{file_bbs_seq}");
+			INTO_COLUMNS("file_origin_name").INTO_VALUES("#{file_origin_name}");
+			INTO_COLUMNS("file_name").INTO_VALUES("#{file_name}");
+		}};
+		return sql.toString();
+	}
+	
+}
---
+++ spMVC_CRUD_06_BBS_04/src/main/java/com/biz/bbs/model/BbsVO.java
@@ -0,0 +1,30 @@
+package com.biz.bbs.model;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BbsVO {
+	private long bbs_seq;//	NUMBER	Primary Key,
+	private long bbs_main_seq;//	NUMBER	,
+	private int bbs_layer;//	NUMBER	,
+	private String bbs_date;//	VARCHAR2(10)	,
+	private String bbs_time;//	VARCHAR2(20)	,
+	private String bbs_auth;//	nVARCHAR2(50)	,
+	private String bbs_title;//	nVARCHAR2(50)	,
+	private String bbs_content;//	nVARCHAR2(50)
+	
+	private List<MultipartFile> bbs_files;
+	
+}
